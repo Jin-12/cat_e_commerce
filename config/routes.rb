04d563cc4b04a91871c173aaaa94b187be9327e0
Jin <coutjules@free.fr>
2019-03-04 14:33:06 +0100

    heroku try

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'item/index'
+  root 'item#index'
   get 'item/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
