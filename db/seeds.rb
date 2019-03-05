c5c9c51b4e7ff0701d02098e445d668d2652294e
Jin <coutjules@free.fr>
2019-03-05 13:49:30 +0100

    le mystere du seeds perdu

---
+++ db/seeds.rb
@@ -0,0 +1,14 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+5.times do
+User.create(email: Faker::Internet.email, password: "mypassword", password_confirmation: "mypassword")
+end
+
+20.times do
+Item.create(title: Faker::Name.middle_name ,description: Faker::Lorem.paragraph , price: rand(1..100), image_url: 'http://golem13.fr/wp-content/uploads/2017/11/chat-double-face-chim%C3%A8re-700x460.jpg')
+end
