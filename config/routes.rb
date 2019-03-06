11174c1ca149b875a265b459eabcd3d8fe3a946f
Aurelien Verschueren <aurelienverschueren@gmail.com>
2019-03-06 10:04:25 +0100

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'orders/create'
+  get 'orders/new'
+  get 'orders/show'
   devise_for :users
   root 'item#index'
   resources :item, only: [:show]
   resources :users, only: [:index, :show, :edit, :create]
-  resources :carts, only: [:show]
+  resources :carts, only: [:show] do
+    resources :orders
+  end
 end
