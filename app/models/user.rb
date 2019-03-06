d4cdf94e1335d9ad67c63bb5d5291bb4cf662606
Aurelien Verschueren <aurelienverschueren@gmail.com>
2019-03-06 12:43:12 +0100

    modif style boutons et page show item

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_one :cart
   has_many :orders
+
+after_create :set_cart
+
+
+  def set_cart
+    Cart.create(user_id: current_user.id)
+  end 
 end
-  
