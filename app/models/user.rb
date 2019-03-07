5bb27c5677c764ac48230b9e696aa3144bae6a97
KuzniClem <clement@kuzniewycz.fr>
2019-03-07 16:55:42 +0100

    s missing in user model has_many orderS

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one :cart
-  has_many :order
+  has_many :orders
 
   def to_param
     [id, /\A\w{1,5}/.match(email)].join("-")
